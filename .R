05c3d14bea00f8f7a7c27107b9c8849d374019ca
waqas-barakzai <waqaskhalid2711@gmail.com>
2026-09-01 13:34:49 +0200

    Update .R

--- .R
+++ .R
@@ -1,40 +1,138 @@
-------------------------------------------------------------------------------------------------------
-#RECONSTRUCTION OF VARIABLES - NEW TRENDS - success code
-library("WaveletComp")
-library("fields")
-my.w <- analyze.wavelet(All_Data_Ready_for_analysis, "lnco2i",loess.span = 0,dt = 0.25, dj = 1/250,make.pval = TRUE, n.sim = 30)
-my.rec <- reconstruct(my.w, plot.waves = FALSE, lwd = c(1,2), legend.coords = "bottomleft", ylim = c(Min=9.21, Max=16.71))
+```r
+# ------------------------------------------------------------
+# RECONSTRUCTION OF VARIABLE A - NEW TRENDS
+# ------------------------------------------------------------
+
+library(WaveletComp)
+library(fields)
+
+# Assign the variable to A
+A <- All_Data_Ready_for_analysis[[1]]
+
+# Wavelet analysis
+my.w <- analyze.wavelet(
+  data.frame(A = A),
+  "A",
+  loess.span = 0,
+  dt = 0.25,
+  dj = 1/250,
+  make.pval = TRUE,
+  n.sim = 30
+)
+
+# Reconstruction
+my.rec <- reconstruct(
+  my.w,
+  plot.waves = FALSE,
+  lwd = c(1, 2),
+  legend.coords = "bottomleft",
+  ylim = c(Min = 9.21, Max = 16.71)
+)
 
 
-------------------------------------------------------------------------------------------------
-  #POWER SPECTURM - success code
-  
-library(biwavelet) 
-attach (my.rec[["series"]])
-A=co2.rec
-B=co2.r
-t1 = cbind(1:480, co2) 
-t2 = cbind(1:480, co2.r) 
-wtc.AB = wtc(t1, t2, nrands = 20) 
-par(oma = c(0, 0, 0, 1), mar = c(5, 4, 4, 5) + 0.1) 
-plot(wtc.AB, plot.phase = FALSE, lty.coi = 1, col.coi = "grey", lwd.coi = 2, lwd.sig = 2, arrow.lwd = 0.03, arrow.len = 0.12, ylab = "Scale", xlab = "Period", plot.cb = TRUE, main = "Industrial CO2 Emissions", xaxt="n")
-n = length(t1[, 1])
-axis(side = 1, at = c(seq(1, n, 24)), labels = c(seq(2004, 2023, 1)))
+# ------------------------------------------------------------
+# POWER SPECTRUM
+# ------------------------------------------------------------
 
-------------------------------------------------------------------------------------------------------
-#wavelet coherence pairs - success coding
-  install.packages("readxl")
 library(biwavelet)
-attach(All_Data_Ready_for_analysis)
 
-A=co2  
-B=gdp
-t1 = cbind(1:480, co2)
-t2 = cbind(1:480, gdp)
-wtc.AB = wtc(t1, t2, nrands = 20)
-par(oma = c(0, 0, 0, 1), mar = c(5, 4, 4, 5) + 0.1)
-plot(wtc.AB, plot.phase = TRUE, lty.coi = 1, col.coi = "grey", lwd.coi = 2, 
-     lwd.sig = 2, arrow.lwd = 0.03, arrow.len = 0.12, ylab = "Scale", xlab = "Period", 
-     plot.cb = TRUE, main = "a. CO2 emissions vs Artificial Intelligence", xaxt="n")
-n = length(t1[, 1])
-axis(side = 1, at = seq(24, n, by = 24), labels = seq(2004, 2023, 1))
+# Extract reconstructed series
+rec.series <- my.rec[["series"]]
+
+A <- rec.series[[1]]
+B <- rec.series[[2]]
+
+# Create time-series objects
+t1 <- cbind(1:480, A)
+t2 <- cbind(1:480, B)
+
+# Wavelet coherence
+wtc.AB <- wtc(
+  t1,
+  t2,
+  nrands = 20
+)
+
+# Plot
+par(
+  oma = c(0, 0, 0, 1),
+  mar = c(5, 4, 4, 5) + 0.1
+)
+
+plot(
+  wtc.AB,
+  plot.phase = FALSE,
+  lty.coi = 1,
+  col.coi = "grey",
+  lwd.coi = 2,
+  lwd.sig = 2,
+  arrow.lwd = 0.03,
+  arrow.len = 0.12,
+  ylab = "Scale",
+  xlab = "Period",
+  plot.cb = TRUE,
+  main = "Variable A",
+  xaxt = "n"
+)
+
+n <- length(t1[, 1])
+
+axis(
+  side = 1,
+  at = c(seq(1, n, 24)),
+  labels = c(seq(2004, 2023, 1))
+)
+
+
+# ------------------------------------------------------------
+# WAVELET COHERENCE PAIR: A vs B
+# ------------------------------------------------------------
+
+install.packages("readxl")
+library(biwavelet)
+
+# Assign the two variables
+A <- All_Data_Ready_for_analysis[[1]]
+B <- All_Data_Ready_for_analysis[[2]]
+
+# Create time-series objects
+t1 <- cbind(1:480, A)
+t2 <- cbind(1:480, B)
+
+# Wavelet coherence
+wtc.AB <- wtc(
+  t1,
+  t2,
+  nrands = 20
+)
+
+# Plot
+par(
+  oma = c(0, 0, 0, 1),
+  mar = c(5, 4, 4, 5) + 0.1
+)
+
+plot(
+  wtc.AB,
+  plot.phase = TRUE,
+  lty.coi = 1,
+  col.coi = "grey",
+  lwd.coi = 2,
+  lwd.sig = 2,
+  arrow.lwd = 0.03,
+  arrow.len = 0.12,
+  ylab = "Scale",
+  xlab = "Period",
+  plot.cb = TRUE,
+  main = "A vs B",
+  xaxt = "n"
+)
+
+n <- length(t1[, 1])
+
+axis(
+  side = 1,
+  at = seq(24, n, by = 24),
+  labels = seq(2004, 2023, 1)
+)
+```
